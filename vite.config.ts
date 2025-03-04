d85984d155102ff820be3125d24c76074ab698c4
ROMA0589 <slipknot19771989@gmail.com>
2025-03-04 10:54:37 -0300

    feat: agregada dependencia @vercel/node

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
-    sourcemap: true,
-    minify: 'esbuild'
+    sourcemap: true
   }
 })
